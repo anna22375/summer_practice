a4fe584ad1015a7e1d7ee8612cce9e8d742b2c22
anuta razbitneva <anna22375@gmail.com>
2017-06-30 08:13:58 +0300

    add GUI

--- GUI.java
+++ GUI.java
@@ -1,24 +1,33 @@
 package sample.summer_practice;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import sample.summer_practice.GUI1;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import sample.summer_practice.Visual;
 
 /** Класс графического интерфейса */
 public class GUI extends JFrame
 {
-    /** Задать длину h и ширину w окна, две кнопки button1, button2 и текст label */
+    /**
+     * Задать длину h и ширину w окна, две кнопки button1, button2 и текст label
+     */
     private static int scale = 2;
+    private static int maxCount = 50;
+
     private int w = 360 * scale, h = 270 * scale;
+    private int spd = 0;
+    private int pse = 0;
+    private int maxLenArr = 10;
 
     private boolean flag = false;
 
     private String tmp;
-    private String[] array;
-
-    private ImageIcon img = new ImageIcon("Images/tree.jpg");
-    private ImageIcon image = new ImageIcon("Images/circles.jpg");
+    private int[] array = new int[maxLenArr];
 
     private JButton button1 = new JButton("Сгенерировать");
     private JButton button2 = new JButton("Ввести вручную");
@@ -27,13 +36,14 @@ public class GUI extends JFrame
     private JButton pause = new JButton("Пауза");
 
     private JLabel label = new JLabel("Как задать массив?");
-    private JLabel tree = new JLabel(img);
-    private JLabel circle = new JLabel(image);
 
     private JTextField text = new JTextField("Введите массив");
-    private JTextField speed = new JTextField("Введите скорость визуализации");
+    private JTextField speed = new JTextField("Введите задержку визуализации");
+    private JTextField ps = new JTextField("Введите длину паузы");
 
-    /** Конструктор класса */
+    /**
+     * Конструктор класса
+     */
     public GUI()
     {
         super();
@@ -50,11 +60,11 @@ public class GUI extends JFrame
 
         /** Размеры и положение кнопок */
         button1.setSize(100 * scale, 20 * scale);
-        button1.setLocation(70 * scale,70 * scale);
-        /** button1.addActionListener(new Action2()); */
+        button1.setLocation(70 * scale, 70 * scale);
+        button1.addActionListener(new Action2());
 
         button2.setSize(100 * scale, 20 * scale);
-        button2.setLocation(210 * scale,70 * scale);
+        button2.setLocation(210 * scale, 70 * scale);
         button2.addActionListener(new Action1());
 
         /** Размеры и положение текста */
@@ -68,16 +78,22 @@ public class GUI extends JFrame
         container.add(button2);
     }
 
-    /** Поведения кнопки генерации массива */
+    /**
+     * Поведения кнопки генерации массива
+     */
     class Action implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
+            spd = Integer.parseInt(speed.getText());
+            pse = Integer.parseInt(ps.getText());
+
             label.setVisible(true);
             button1.setVisible(true);
             button2.setVisible(true);
 
             speed.setVisible(false);
+            ps.setVisible(false);
             next.setVisible(false);
 
             label.setText("Выберите тип визуализации");
@@ -87,22 +103,20 @@ public class GUI extends JFrame
 
             button2.setText("Размер");
             button2.addActionListener(new Action4());
-
-            tree.setSize(150 * scale, 110 * scale);
-            tree.setLocation(45 * scale, 95 * scale);
-
-            circle.setSize(150 * scale, 110 * scale);
-            circle.setLocation(185 * scale, 95 * scale);
         }
     }
 
-    /** Поведение кнопки ручного ввода массива */
+    /**
+     * Поведение кнопки ручного ввода массива
+     */
     class Action1 implements ActionListener
     {
-        public void  actionPerformed(ActionEvent e)
+        public void actionPerformed(ActionEvent e)
         {
+            flag = true;
+
             text.setSize(120 * scale, 25 * scale);
-            text.setLocation(210 * scale,150 * scale);
+            text.setLocation(210 * scale, 150 * scale);
 
             nexter.setSize(30 * scale, 15 * scale);
             nexter.setLocation(300 * scale, 210 * scale);
@@ -117,9 +131,45 @@ public class GUI extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
-            tmp = text.getText();
+            if (flag == true)
+            {
+                tmp = text.getText();
 
-            array = tmp.split(" ");
+                String[] arr = tmp.split(" ");
+
+                if (arr.length > maxLenArr)
+                {
+                    int kostyl = Integer.parseInt("kostyl");
+                }
+
+                for (int i = 0; i < arr.length; i++) if(Integer.parseInt(arr[i]) > maxCount)
+                {
+                    int kostyl = Integer.parseInt("kostyl");
+                }
+
+                for (int i = 0; i < arr.length; i++) array[i] = Integer.parseInt(arr[i]);
+            }
+            else
+            {
+                Test test = new Test(maxLenArr);
+
+                try
+                {
+                    Scanner scanner = new Scanner(new File("in.txt"));
+
+                    int i = 0;
+
+                    while(scanner.hasNextInt()) array[i++] = scanner.nextInt();
+                }
+                catch (FileNotFoundException er)
+                {
+                    System.out.println("Sorry, File not found!");
+                }
+                catch (InputMismatchException er)
+                {
+                    System.out.println("Sorry, InputMismatchException");
+                }
+            }
 
             label.setVisible(false);
             button1.setVisible(false);
@@ -128,13 +178,17 @@ public class GUI extends JFrame
             nexter.setVisible(false);
 
             speed.setSize(120 * scale, 25 * scale);
-            speed.setLocation(120 * scale,70 * scale);
+            speed.setLocation(120 * scale, 70 * scale);
+
+            ps.setSize(120 * scale, 25 * scale);
+            ps.setLocation(120 * scale, 100 * scale);
 
             next.setSize(30 * scale, 15 * scale);
-            next.setLocation(165 * scale, 100 * scale);
+            next.setLocation(165 * scale, 130 * scale);
             next.addActionListener(new Action());
 
             add(speed);
+            add(ps);
             add(next);
         }
     }
@@ -143,7 +197,39 @@ public class GUI extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
+            Visual scene = new Visual(array, spd);
 
+            JPanel cp = new JPanel(new BorderLayout());
+
+            cp.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5),
+                    BorderFactory.createLineBorder(Color.black)));
+
+            setContentPane(cp);
+
+            add(scene, BorderLayout.CENTER);
+
+            pause.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try
+                    {
+                        Thread.sleep(pse);
+                    }
+
+                    catch (InterruptedException err)
+                    {
+                        err.printStackTrace();
+                    }
+                }
+            });
+
+            cp.add(pause, BorderLayout.SOUTH);
+
+            setSize(250 * scale, 200 * scale);
+            setLocationRelativeTo(null);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setVisible(true);
         }
     }
 
@@ -151,7 +237,40 @@ public class GUI extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
+            Visualisation scene = new Visualisation(array, spd);
 
+            JPanel cp = new JPanel(new BorderLayout());
+
+            cp.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5),
+                    BorderFactory.createLineBorder(Color.black)));
+
+            setContentPane(cp);
+
+            add(scene, BorderLayout.CENTER);
+
+            pause.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try
+                    {
+                        Thread.sleep(pse);
+                    }
+
+                    catch (InterruptedException err)
+                    {
+                        err.printStackTrace();
+                    }
+                }
+            });
+
+            cp.add(pause, BorderLayout.SOUTH);
+            cp.setBackground(Color.white);
+
+            setSize(250 * scale, 200 * scale);
+            setLocationRelativeTo(null);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setVisible(true);
         }
     }
 }
